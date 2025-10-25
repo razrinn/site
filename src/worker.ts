81937c7025afb6b25d3f19c93a3e4babf3cfab1a
Ray Azrin Karim <ray.karim@stockbit.com>
2025-10-25 13:13:03 +0700

    add response

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,5 @@
 export default {
-  fetch(request) {
+  async fetch(request, env) {
     const url = new URL(request.url)
 
     if (url.pathname.startsWith('/giscus')) {
@@ -12,6 +12,19 @@ export default {
       if (request.method === 'OPTIONS') {
         return new Response(null, {
           headers: corsHeaders,
+        })
+      }
+
+      if (request.method === 'GET') {
+        const response = await env.ASSETS.fetch(request)
+        const newHeaders = new Headers(response.headers)
+        Object.entries(corsHeaders).forEach(([key, value]) => {
+          newHeaders.set(key, value)
+        })
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: newHeaders,
         })
       }
     }
